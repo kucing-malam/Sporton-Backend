b129cb8d93dca089ed0b9809a3514600921d6539
ferrypradana07 <ferrykun500@gmail.com>
2026-02-03 17:37:21 +0700

    refactor: make purchasedItems to interface and schema

--- src/models/transaction.model.ts
+++ src/models/transaction.model.ts
@@ -1,23 +1,33 @@
 import mongoose, { Schema, Document } from "mongoose";
 
+export interface IPurchasedItem {
+    productId: mongoose.Types.ObjectId,
+    qty: number
+}
+
 export interface ITransaction extends Document {
     imageUrl: string;
-    purchasedItems: string[];
+    purchasedItems: IPurchasedItem[];
     totalPayment: number;
     customerName: string;
     customerContact: string;
     customerAddress: string;
-    status: string;
+    status: "pending" | "paid" | "rejected";
 }
 
+const PurchasedItemSchema: Schema = new Schema({
+    purchasedItems: { type: [mongoose.Types.ObjectId], require: true, ref: "Product" },
+    qty: { type: Number, min: 1, required: true }
+}, { _id: false })
+
 const TransactionSchema: Schema = new Schema({
-    imageUrl: {type: String, require: true},
-    purchasedItems: {type: [mongoose.Types.ObjectId], require: true, ref: "Product"},
-    totalPayment: {type: Number, require: true},
-    customerName: {type: String, require: true},
-    customerContact: {type: String, require: true},
-    customerAddress: {type: String, require: true},
-    status: {type: String, require: true},
+    imageUrl: { type: String, require: true },
+    purchasedItems: { type: PurchasedItemSchema, require: true, ref: "Product" },
+    totalPayment: { type: Number, require: true },
+    customerName: { type: String, require: true },
+    customerContact: { type: String, require: true },
+    customerAddress: { type: String, require: true },
+    status: { type: String, enum: ["pending", "rejected", "paid"], require: true }
 }, { timestamps: true })
 
 export default mongoose.model<ITransaction>("Transaction", TransactionSchema)

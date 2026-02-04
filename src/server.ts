7b1c900aecb72634a8aaba8271b43456868e65fe
ferrypradana07 <ferrykun500@gmail.com>
2026-02-04 19:30:22 +0700

    add validate type for port

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,13 @@ import dotenv from "dotenv"
 import app from "./app"
 
 dotenv.config() 
-const PORT = process.env.PORT || "3000"
-const MONGO_URI = process.env.MONGO_URI || "no-mongo_uri"
+const PORT = process.env.PORT || 3000
+const MONGO_URI = process.env.MONGO_URI  || "no-mongo_uri"
 
 mongoose.connect(MONGO_URI as string)
     .then(() => {
         console.log("Connected to MongoDB")
-        app.listen(PORT, () => {
+        app.listen(PORT as number, "0.0.0.0", () => {
             console.log("Server is running on ", PORT)
         })
     })    
